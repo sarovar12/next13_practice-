e03706f8afbb1e1293c811bc8d89b07eabb994a6
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-07-05 20:19:06 +0545

    Made Menu Dynamic

--- src/app/resturant/[slug]/menu/page.tsx
+++ src/app/resturant/[slug]/menu/page.tsx
@@ -3,16 +3,35 @@ import React from 'react'
 import ResturantNavbar from '../components/ResturantNavbar'
 import Menu from './components/Menu'
 import { Metadata } from "next";
+import { PrismaClient } from '@prisma/client';
+
 export const metadata: Metadata = {
     title: ' Menu | Corrine Bar (Nakkhu) | OpenTable',
   }
+const prisma = new PrismaClient();
+const fetchRestaurantItems = async ( slug:string)=>{
+  const restaurant = await prisma.restaurant.findUnique({
+    where:{
+      slug
+    },
+    select:{
+      items:true,
+    }
+  })
 
-export default function ResturantMenu() {
+  if(!restaurant){
+    throw new Error
+  }
+  return restaurant.items
+}
+
+export default async function ResturantMenu({params}:{params:{slug:string}}) {
+  const menu = await fetchRestaurantItems(params.slug)
   return (
     <>  
         <div className="bg-white  w-[100%] rounded p-3 shadow">
-        <ResturantNavbar/>
-        <Menu/>
+        <ResturantNavbar slug={params.slug} />
+        <Menu menu={menu} />
          </div>
     </>
   )
