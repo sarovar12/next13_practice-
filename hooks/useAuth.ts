ceff404f5bc057a5002365942448ba155deccc4b
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2023-07-27 16:31:18 +0545

    Handled error and loading states after entering credentials

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,17 +1,35 @@
+import { AuthenticationContext } from "@/app/context/authContext";
 import axios from "axios";
+import { useContext } from "react";
+
 const useAuth =()=>{
+    const {data,error,loading,setAuthState } = useContext(AuthenticationContext)
     const signIn = async ({
         email,password
     }:{email:string;password:string}
     )=>{
+        setAuthState({
+            data:null,
+            error:null,
+            loading:true,
+        })
         try {
           const response =  await axios.post("http://localhost:3000/api/auth/signin",{
                 email,
                 password
             })
-            console.log(response)
-        } catch (error) {
-            console.log(error)
+            setAuthState({
+                data:response.data,
+                error:null,
+                loading:false,
+            })
+        } catch (error:any ) {
+            // console.log(error.response.data.errorMessage)
+            setAuthState({
+                data:null,
+                error:error.response.data.errorMessage,
+                loading:false,
+            })
         }
     }
     const signUp = async ()=>{
--- src/app/components/AuthModal.tsx
+++ src/app/components/AuthModal.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState, useContext } from 'react';
 import AuthModalInput from './AuthModalInput';
 import useAuth from '../../../hooks/useAuth';
 import { AuthenticationContext } from '../context/authContext';
+import { Alert, CircularProgress } from '@mui/material';
 
 
 const style = {
@@ -22,7 +23,7 @@ const style = {
 };
 
 export default function AuthModal({isSignIn}:{isSignIn:boolean}) {
-  const {error,loading,data,setAuthState } = useContext(AuthenticationContext)
+  const {error,loading,data } = useContext(AuthenticationContext);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -81,38 +82,45 @@ export default function AuthModal({isSignIn}:{isSignIn:boolean}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-            <div className="p-2 h-[490px]">
-              
-                <div className="uppercase font-bold text-center pb-2 border-b mb-2">
-                    <div className="text-sm">
-                        {renderContent("Sign In","Create Account")}
-                        {/* <h1 onClick={()=>{
-                          setAuthState({
-                          data,loading,
-                          error:"Bye Bye"
-                        })}}>
-                          {error}
-                          </h1> */}
-                    </div>
-                </div>
-                <div className=' m-auto '>
-                    <h2 className='text-2xl font-light text-center'>
-                        {renderContent("Log Into Your Account","Create your Open Table Account")}
-                    </h2>
-                    <AuthModalInput input={inputs}
-                    handleChangeInput={handleChangeInput} 
-                    isSignIn={isSignIn}/>
-                    <button
-                    disabled={disabled} 
-                    onClick={handleClick}
-                    className=' uppercase bg-red-600 w-full text-white p-3 rounded
-                    text-sm mb-5 disabled:bg-gray-400'>
-                      {
-                        renderContent("Sign In","Create Account")
-                      }
-                    </button>
-                </div>
-            </div>
+
+{  loading?
+<div className="px-2 py-24 h-[490px] flex justify-center">
+<CircularProgress/>
+</div>
+  :<div className="p-2 h-[490px]">
+          {error?
+            <Alert className='mb-4' severity="error">{error}</Alert>:
+            null}
+              <div className="uppercase font-bold text-center pb-2 border-b mb-2">
+                  <div className="text-sm">
+                      {renderContent("Sign In","Create Account")}
+                      {/* <h1 onClick={()=>{
+                        setAuthState({
+                        data,loading,
+                        error:"Bye Bye"
+                      })}}>
+                        {error}
+                        </h1> */}
+                  </div>
+              </div>
+              <div className=' m-auto '>
+                  <h2 className='text-2xl font-light text-center'>
+                      {renderContent("Log Into Your Account","Create your Open Table Account")}
+                  </h2>
+                  <AuthModalInput input={inputs}
+                  handleChangeInput={handleChangeInput} 
+                  isSignIn={isSignIn}/>
+                  <button
+                  disabled={disabled} 
+                  onClick={handleClick}
+                  className=' uppercase bg-red-600 w-full text-white p-3 rounded
+                  text-sm mb-5 disabled:bg-gray-400'>
+                    {
+                      renderContent("Sign In","Create Account")
+                    }
+                  </button>
+              </div>
+          </div>}
         </Box>
       </Modal>
     </div>
